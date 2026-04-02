5227daed823d499c06db3d489408779620b6e952
sun <sunwoo4916@gmail.com>
2026-04-02 09:53:13 +0900

    2

---
+++ devel/HelloWorld.java
@@ -0,0 +1,12 @@
+// 주석
+
+/*
+주석
+주석
+*/
+
+public class HelloWorld{
+    public static void main(String[] arg){
+        System.out.print("hello!");
+    }
+}
---
+++ devel/Student.java
@@ -0,0 +1,6 @@
+public class Student {
+    public static void main(String[] arg){
+        System.out.println("이름 : 김선우");
+        System.out.print("성별 : 남 자");
+    }
+}
