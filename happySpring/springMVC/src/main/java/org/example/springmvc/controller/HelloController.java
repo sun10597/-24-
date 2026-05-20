1d57fc16d6e8968c5fdb472cedba0369f08971a9
sun <sunwoo4916@gmail.com>
2026-05-20 17:20:57 +0900

    0520

---
+++ happySpring/springMVC/src/main/java/org/example/springmvc/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.example.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
+}
